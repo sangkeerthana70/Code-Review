0b4412a0ddc95e0af673c0c0e4741a55e29df5a3
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-03 11:49:08 -0400

    Objectified Milo's Draw board solution

--- DrawBoardV2Milo/Program.cs
+++ DrawBoardV2Milo/Program.cs
@@ -12,50 +12,33 @@ namespace DrawBoardV2Milo
         // Great functionality and code simplified
         public static void Main(string[] args)
         {
-            DrawBoard(4, 8);
-        }
+            //DrawBoard(4, 8);
+            Console.WriteLine("Enter height of board between 2 and 30: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            
 
-        private static void drawTop()
-        {
-            Write(" __ ");
-        }
+            Console.WriteLine("Enter width of board between 2 and 30");
+            int width = Convert.ToInt32(Console.ReadLine());
 
-        private static void drawWalls()
-        {
-            Write("|  |");
-        }
 
-        private static void drawBottom()
-        {
-            Write("+--+");
-        }
-
-        /* I really like the way you have simplified the entire draw board challenge. 
-         * But one of the ways you could change the code is objectifying the whole code to separate the static DrawBoard
-         * into a separate class and include your drawTop, drawWalls, drawBottom methods inside the class.
-         * This will help in code reusability and implementing  OOPS concepts for further usage.*/
-         
-        private static void DrawBoard(int height, int width)
-        {
-            for (int rowCounter = 0; rowCounter < height; rowCounter++)
+            if (height < 2 || height > 30)
             {
-                for (int columnCounter = 0; columnCounter < width; columnCounter++)
-                {
-                    drawTop();
-                }
-                Console.WriteLine();
-                for (int wallCounter = 0; wallCounter < width; wallCounter++)
-                {
-                    drawWalls();
-                }
-                Console.WriteLine();
-                // the draw bottom can be changed to draw " __ " which will look like the second draw board image of Cliff's.
-                for (int bottomCounter = 0; bottomCounter < width; bottomCounter++)
-                {
-                    drawBottom();
-                }
-                Console.WriteLine();
+                Console.WriteLine("Please enter a valid height");
             }
+            if (width < 2 || width > 30)
+            {
+                Console.WriteLine("Please enter a valid width");
+            }
+
+            Game myGame = new Game(height, width);
+            myGame.DrawBoard(height, width);
         }
+
+       
+            
+           
+               
+          
+
     }
 }
